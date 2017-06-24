1f4a26cbc955b6fad2ba626b3e67b9eec53de592
Martha <suerte.0125@gmail.com>
2017-06-24 14:37:11 -0700

    add javascript/jquery

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$("document").ready(function() {
+  $("form#count").submit(function(event) {
+    event.preventDefault();
+
+    var number = 0;
+    var count = $("input#number").val();
+    alert("hello");
+    console.log (0);
+    var result = $("input#count").val
+
+    $("#output").text("ping");
+  });
+});
