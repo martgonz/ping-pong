38059c0a83bd1dfd187db5aac85e6e1d752daef5
Martha <suerte.0125@gmail.com>
2017-06-25 11:35:04 -0700

    changed some jquery/javascript hoping to fix coding, still having issues.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,21 @@
-$("document").ready(function() {
+$(document).ready(function() {
   $("form#count").submit(function(event) {
     event.preventDefault();
     var count = parseInt($("input#count").val());
     var result = newNumber(count);
     $("#output").show();
-  }
-  var newNumber = parseInt($("input#count").val());
-  //console.log (newNumber);
-  var submit = newNumber(count = ++1);
-  $("count" = % 3).parseInt();
-  return parseInt()
-  $("#output").show(ping);
-  if $("count" = % 5).parseInt();
-  return parseInt()
-  $("#output").show(pong);
-} else {
-  var newNumber = function(count) {
-    if ((count % 3 === 0) && (count % 5 === 0) {
-      return.prompt("pingpong");
+    //  console.log (newNumber);
+    var newNumber = parseInt($("input#count").val());
+    var submit = newNumber(count = +1);
+    $("count" / 3).parseInt();
+    return parseInt()
+    $("#output").show(ping);
+    if $("count" / 5).parseInt();
+    return parseInt()
+    $("#output").show(pong);
+  } else {
+    var newNumber = function(count) {
+      if ((count / 3 === 0) && (count / 5 === 0) {
+        return.prompt("pingpong");
+      });
     });
-  });
